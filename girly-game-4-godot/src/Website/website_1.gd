995c4c8f12575bd8d84a1d335d85484976cfa5b4
dijimmi <arieljose2005@gmail.com>
2026-08-10 07:14:56 -0400

    made undo/redo work better 👍

--- girly-game-4-godot/src/Website/website_1.gd
+++ girly-game-4-godot/src/Website/website_1.gd
@@ -17,16 +17,23 @@ var stack = [
 func _ready() -> void:
 	products_page.product_clicked_from_category.connect(view_product)
 	product_big_view.color_changed.connect(_on_color_changed)
+	toggle_undo_and_redo()
 
 
-func view_product(dict, index = 0):
-	
+func view_product(dict, index = 0, from_redo = false):
 	products_page.visible = false
 	view_product_page.visible = true
 	
+	if not from_redo:
+		stack.clear()
+		stack_idx = 0
+		stack.append([dict, index])
+		
 	product_big_view.setup(dict, index)
-	stack.append([dict, index])
-	stack_idx += 1
+	
+	print("normal view pressed: %s" % stack_idx)
+	
+	toggle_undo_and_redo()
 
 
 func _on_color_changed(index):
@@ -36,18 +43,28 @@ func _on_color_changed(index):
 
 func _on_undo_pressed() -> void:
 	stack_idx -= 1
+	print("undo pressed: %s" % stack_idx)
 	view_webpage()
+	toggle_undo_and_redo()
 
 
 func view_webpage():
 	products_page.visible = true
 	view_product_page.visible = false
+	
+
+
+func toggle_undo_and_redo():
+	redo.disabled = stack_idx >= (stack.size() - 1)
+	undo.disabled = stack_idx < 0
 
 
 func _on_redo_pressed() -> void:
 	stack_idx += 1
+	print("redo pressed: %s" % stack_idx)
 	if stack_idx < stack.size():
+		
 		var product = stack[stack_idx][0]
 		var index = stack[stack_idx][1]
 
-		view_product(product, index)
+		view_product(product, index, true)
