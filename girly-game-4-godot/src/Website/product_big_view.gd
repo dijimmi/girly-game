d936d776b299d239099ea26303692b9997477404
dijimmi <arieljose2005@gmail.com>
2026-08-10 06:56:08 -0400

    implemented undo and redo buttons

--- girly-game-4-godot/src/Website/product_big_view.gd
+++ girly-game-4-godot/src/Website/product_big_view.gd
@@ -5,21 +5,29 @@ extends VBoxContainer
 @export var top_img : TextureRect
 
 @export var color_option : PackedScene
-@export var colors : VBoxContainer
+@export var colors : HBoxContainer
 
 var current_product : Dictionary
+var current_color_index : int = 0
+
+signal color_changed(index : int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 
-func setup(product):
+func setup(product, index = 0):
 	current_product = product
 	
 	base_img.texture = product['base_texture']
 	tint_img.texture = product['tint_texture']
 	top_img.texture = product['top_texture']
+	
+	change_color(product, index)
+	
+	for color in colors.get_children():
+		color.queue_free()
 	
 	for i in product['base_colors'].size():
 		add_button(product, i)
@@ -40,6 +48,9 @@ func add_button(product, index):
 
 
 func change_color(product, index):
+	current_color_index = index
+	color_changed.emit(index)
+	
 	base_img.modulate = product['base_colors'][index]
 	tint_img.modulate = product['tint_colors'][index]
 	top_img.modulate = product['top_colors'][index]
--- girly-game-4-godot/src/Website/website_1.gd
+++ girly-game-4-godot/src/Website/website_1.gd
@@ -4,13 +4,50 @@ extends Control
 @export var view_product_page : HBoxContainer
 @export var product_big_view : VBoxContainer
 
+@export var undo : Button
+@export var redo : Button
+
+var stack_idx = -1
+
+var stack = [
+]
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	products_page.product_clicked_from_category.connect(view_product)
+	product_big_view.color_changed.connect(_on_color_changed)
 
 
-func view_product(dict):
+func view_product(dict, index = 0):
+	
 	products_page.visible = false
 	view_product_page.visible = true
 	
-	product_big_view.setup(dict)
+	product_big_view.setup(dict, index)
+	stack.append([dict, index])
+	stack_idx += 1
+
+
+func _on_color_changed(index):
+	await get_tree().process_frame
+	stack[stack_idx][1] = index
+
+
+func _on_undo_pressed() -> void:
+	stack_idx -= 1
+	view_webpage()
+
+
+func view_webpage():
+	products_page.visible = true
+	view_product_page.visible = false
+
+
+func _on_redo_pressed() -> void:
+	stack_idx += 1
+	if stack_idx < stack.size():
+		var product = stack[stack_idx][0]
+		var index = stack[stack_idx][1]
+
+		view_product(product, index)
