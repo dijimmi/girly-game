13616111d61c0aca40b2e93fdce7011261d77f2d
Astro <astridlo131@gmail.com>
2026-08-14 15:03:08 +0200

    SceneSelector

---
+++ girly-game-4-godot/src/UI/MainMenuUI/level_selector.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var level_handler : Node
+
+func _ready():
+	hide()
+	level_handler = get_tree().root.get_node("Game/LevelHandler")
+
+func open_level_selector():
+	show()
+	if !level_handler:
+		return
+	for scene in level_handler.scene_array.keys():
+		var new_button := Button.new()
+		new_button.text = scene
+		$GridContainer.add_child(new_button)
+		new_button.pressed.connect(func ():
+			EventBus.load_scene.emit(scene))
+
+func close_level_selector():
+	hide()
