77f92a8813a42409dafbe4adb9bffb76987cd08b
Astro <astridlo131@gmail.com>
2026-08-14 23:50:16 +0200

    Dialogic audio settings small modifs

--- girly-game-4-godot/src/UI/MainMenuUI/settings.gd
+++ girly-game-4-godot/src/UI/MainMenuUI/settings.gd
@@ -18,11 +18,12 @@ func _on_back_button_pressed() -> void:
 func _on_volume_h_slider_value_changed(value: float) -> void:
 	var indx = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_volume_db(indx, value)
-
+func _on_voices_h_slider_2_value_changed(value: float) -> void:
+	var indx = AudioServer.get_bus_index("Voices")
+	AudioServer.set_bus_volume_db(indx, value)
 func _on_music_h_slider_value_changed(value: float) -> void:
 	var indx = AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_volume_db(indx, value)
-
 func _on_sound_effects_h_slider_value_changed(value: float) -> void:
 	var indx = AudioServer.get_bus_index("SoundEffects")
 	AudioServer.set_bus_volume_db(indx, value)
